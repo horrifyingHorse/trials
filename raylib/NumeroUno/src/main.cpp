0a0834306fb16d7ff9a6c2ad31b8f1f3cffb0fd8
horrifyingHorse <excuseplanation@gmail.com>
2025-01-31 01:31:07 +0530

    bouncing them balls

--- raylib/NumeroUno/src/main.cpp
+++ raylib/NumeroUno/src/main.cpp
@@ -1,28 +1,79 @@
 #include <raylib.h>
+#include <string>
+
+#define BGCOLOR (Color){23, 23, 23, 255}
+#define FGCOLOR (Color){244, 221, 222, 255}
+
+class Bird {
+public:
+  Bird(int radius, Color c = {0, 0, 0, 255}) {
+    birb.r = radius;
+    birb.x = GetScreenWidth() / 2;
+    birb.y = GetScreenHeight() / 2;
+    birb.c = c;
+  }
+
+  void draw() { DrawCircle(birb.x, birb.y, birb.r, birb.c); }
+
+  void flap() { t = -5; }
+
+  void gravity() {
+    if (birb.y >= GetScreenHeight() - birb.r && t >= 0) {
+      birb.y = GetScreenHeight() - birb.r;
+      velocity = 0;
+      t = 0;
+      return;
+    }
+    velocity = acceleration * t;
+    t += 0.2;
+    birb.y += velocity;
+  }
+
+  void debug() {
+    std::string v = "velocity: " + std::to_string(velocity);
+    std::string tstr = "t: " + std::to_string(t);
+    DrawText(v.c_str(), 100, 100, 20, FGCOLOR);
+    DrawText(tstr.c_str(), 100, 130, 20, FGCOLOR);
+  }
+
+protected:
+  typedef struct Circle {
+    int x, y, r;
+    Color c = {0, 0, 0, 255};
+  } Circle;
+
+  Circle birb;
+
+  int velocity;
+  float acceleration = 1;
+  float t = 0;
+};
 
 int main() {
   int h = 500, w = 900;
   InitWindow(w, h, "bilyag");
   SetTargetFPS(60);
-  Rectangle r = {100, 100, 30, 20};
 
+  Bird b(25, FGCOLOR);
   int speed = 10;
+  bool isPressed = false;
   while (!WindowShouldClose()) {
-    if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_L)) && r.x < w - 30)
-      r.x += speed;
-    else if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_H)) && r.x > 0)
-      r.x -= speed;
-    else if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_K)) && r.y > 0)
-      r.y -= speed;
-    else if ((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_J)) && r.y < h - 30)
-      r.y += speed;
+    if (IsKeyDown(KEY_SPACE) && !isPressed) {
+      b.flap();
+      isPressed = true;
+    }
+    if (IsKeyUp(KEY_SPACE)) {
+      isPressed = false;
+    }
 
     BeginDrawing();
-    ClearBackground(SKYBLUE);
-    DrawRectangleRec(r, RED);
-    DrawCircle(r.x + 10, r.y + r.height / 2, 5, BLACK);
-    DrawText("What are these args btw..", 320, 450, 20, BLACK);
+    ClearBackground(BGCOLOR);
+    DrawText("Space To Bounce", 100, 350, 20, FGCOLOR);
+    b.debug();
+    b.draw();
     EndDrawing();
+
+    b.gravity();
   }
 
   CloseWindow();
