0a8e2aa502d654ebe2ea9d1e9424da52764d8dbb
horrifyingHorse <excuseplanation@gmail.com>
2025-06-14 21:22:20 +0530

    Tokenize in go

--- go/gosh/main.go
+++ go/gosh/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	// "reflect"
+
 	// "log"
 	"os"
 	"os/exec"
+
+	"slices"
 	"strings"
 )
 
@@ -24,7 +28,12 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print("gosh> ")
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Failed to get current dir")
+			os.Exit(1)
+		}
+		fmt.Print(cwd + "> ")
 		if !scanner.Scan() {
 			break
 		}
@@ -36,12 +45,28 @@ func main() {
 		//
 		// ans} To implement $status or $?
 		// We need to Tokenize -> Expand Env
-		line := os.ExpandEnv(scanner.Text())
+		line := scanner.Text()
+		line = strings.Trim(line, " \t")
+		fmt.Println(Tokenize(&line), len(Tokenize(&line)))
 		if line == "exit" {
 			break
 		}
 
+		// ls|wc
+		// echo "ls|wc"
+
 		arr := strings.Split(line, " ")
+
+		if arr[0] == "cd" {
+			if len(arr) > 2 {
+				fmt.Println("Too many arguments for cd")
+			} else {
+				if err := os.Chdir(arr[1]); err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+			continue
+		}
 
 		// Explicitly Handle: | < > 2> 1> 2>&1 & && ~ $?
 		cmd := exec.Command(arr[0], arr[1:]...)
@@ -60,4 +85,52 @@ func main() {
 		// 	fmt.Printf("%s", resp)
 		// }
 	}
+}
+
+func Tokenize(s *string) []string {
+	var tokens []string
+	var toParse bool
+	var from int
+
+	toParse = true
+	from = 0
+
+	ignore := strings.Split(" \t", "")
+	d := strings.Split("|><", "")
+	for i, c := range *s {
+		if c == '"' {
+			toParse = !toParse
+			continue
+		}
+
+		if !toParse {
+			continue
+		}
+
+		if slices.Contains(ignore, string(c)) {
+			if i == from {
+				from++
+				continue
+			}
+			tokens = append(tokens, (*s)[from:i])
+			from = i + 1
+			tokens[len(tokens)-1] = os.ExpandEnv(tokens[len(tokens)-1])
+		} else if slices.Contains(d, string(c)) {
+			if i == from {
+				tokens = append(tokens, string(c))
+			} else {
+				tokens = append(tokens, (*s)[from:i], string(c))
+				tokens[len(tokens)-2] = os.ExpandEnv(tokens[len(tokens)-2])
+			}
+			from = i + 1
+		}
+
+	}
+
+	if from != len(*s) {
+		tokens = append(tokens, (*s)[from:])
+		tokens[len(tokens)-1] = os.ExpandEnv(tokens[len(tokens)-1])
+	}
+
+	return tokens
 }
