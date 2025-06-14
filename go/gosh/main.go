485204f4ebe149eab6ca0566f0f23d6d041e6f3c
horrifyingHorse <excuseplanation@gmail.com>
2025-06-14 15:23:07 +0530

    more go today

--- go/gosh/main.go
+++ go/gosh/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
+	// "log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello noobs")
+	// ENV := make(map[string]string)
+	// inheritedEnv := os.Environ()
+	// for _, envVar := range inheritedEnv {
+	// 	if index := strings.Index(envVar, "="); index == -1 {
+	// 		log.Fatal("Invalid Environment Variable Inherited from parent. got:", envVar)
+	// 	} else {
+	// 		ENV[envVar[:index]] = envVar[index+1:]
+	// 	}
+	// }
+	fmt.Println("$HOME=" + os.Getenv("HOME"))
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -19,20 +29,27 @@ func main() {
 			break
 		}
 
-		line := scanner.Text()
+		// Expand Env -> Tokenize
+		// [ O R ]
+		// Tokenize		-> Expand Env
+		// That is the Question
+		//
+		// ans} To implement $status or $?
+		// We need to Tokenize -> Expand Env
+		line := os.ExpandEnv(scanner.Text())
 		if line == "exit" {
 			break
 		}
 
 		arr := strings.Split(line, " ")
 
-		// Explicitly Handle: | < > 2> 1> 2>&1 & &&
+		// Explicitly Handle: | < > 2> 1> 2>&1 & && ~ $?
 		cmd := exec.Command(arr[0], arr[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
+			fmt.Println(err.Error())
 		}
 
 		// or
